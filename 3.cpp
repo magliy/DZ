e03c49d11492c0f67020de70e9b6581dc7fcec7d
magliy <68062800+magliy@users.noreply.github.com>
2020-07-10 17:14:00 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 using namespace std;
-int SQR(int a) {
+void SQR(int a) {
 	int c = 0;
 	for (int i = 1; i*i < a; i = i + 1) {
 		c = i * i;
 		cout << c << ' ';
 	}
-	return  c;
 }
 
+void F(int a) {
+
+	for (int i = 1; i*i <= a; i++) {
+		if (a % i == 0)
+			cout << i << ' ' << a / i << ' ';
+
+	}
+}
+
+void f(int a) {
+	for (int i = 1; i <= a; i++) {
+		if (a % i == 0)
+			cout << i << ' ';
+	}
+}
+
+bool G(int a) {
+	for (int i = 2; i * i <= a; i++) {
+		if (a % i == 0)
+			return false;
+	}
+	return true;
+}
 int main()
-{
-	cout << SQR(4);
+{ 
+	cout << "started" << endl;
+	cout << G(597331883);
+	if (0)
+		cout << "true";
 	return 0;
 }
