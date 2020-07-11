3c10d931b81656ed83a4870519e11aa75640b34d
magliy <68062800+magliy@users.noreply.github.com>
2020-07-11 16:32:31 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,24 +1,24 @@
 int fibonachi(int a) {
 
-	if (a == 0) return;
-	if (a == 1) return;
+	if (a == 0) return 0;
+	if (a == 1) return 1;
 	if (a > 1) {
 		return fibonachi(a - 1) + fibonachi(a - 2);
 	}
 }
 
 int fib(int n) {
-	int array[n] ;
+	int array[n];
 	array[0] = 0;
 	array[1] = 1;
 	for (int i = 2; i <= n; i++) {
 		array[i] = array[i - 1] + array[i - 2];
 	}
-	return array[i];
+	return array[n-1];
 }
 void Array(int n, int x) {
 	int ar[n];
-	for (int i = 0; i >= n; i++) {
+	for (int i = 0; i <= n; i++) {
 		ar[i] = i + x;
 		cout << ar[i] << ' ';
 	}
