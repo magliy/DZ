8199ecf2f43c7708d8d1f413339215c132506a2c
magliy <68062800+magliy@users.noreply.github.com>
2020-07-12 17:57:00 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,30 +1,47 @@
 struct M {
-    int* mas;
-    int current;
-    int stepen;
-    int size;
-    int x;
-    int mnogochlen();
-    M(int n) {
-        mas=new int[n];
-        current=0;
-        size=n;
-    }
-    int mnogochlen(int* mas,int n,int stepen) {
-        for(int i=0;i<n;i++) {
-            return mas[i]*x^stepen+mas[i+1]*x^(stepen-i);
-        }
-    }
-        ~M(){
-            cout<<"end"<<endl;
-            delete[] mas;
-        }
-    };
+	int* mas;
+	int size;
+	int x;
+	int mnogochlen();
+	M(int n) {
+		mas = new int[n];
+		size = n;
+		for (int i = 0; i < n; i++) {
+			mas[i] = 0;
+		}
+	}
+	M(int arr[], int n) {
+		mas = new int[n];
+		size = n;
+		for (int i = 0; i < n; i++) {
+			mas[i] = arr[i];
+		}
+	}
+	int& operator[] (int i) {
+		return mas[i];
+	}
+	
+	int mnogochlen(int x) {
+		int v = 1;
+		int sum = 0;
+		for (int i = 0; i < size; i++) {
+			sum = sum + mas[i] * v;
+			v = v * x;
+		}
+		return sum;
+	}
+		~M() {
+			cout << "end" << endl;
+			delete[] mas;
+		}
+	};
 
-int main() 
-{
-    M v(4);
-    int arr[4]={1,4,5,8};
-    cout<<v.mnogochlen(arr,4,3);
-    return 0;
-}
+	int main()
+	{
+		
+		int arr[4] = { 1,4,5,8 };
+		M v(arr, 4);
+		v[0] = 0;
+		cout << v.mnogochlen(2)<<' ';
+		return 0;
+	}
