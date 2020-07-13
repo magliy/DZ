3553f6241e050629f432e7820d92ef4ed932a178
magliy <68062800+magliy@users.noreply.github.com>
2020-07-13 22:32:04 +0300

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Mas {
+	vector<int> v;
+	int size;
+	void change();
+	Mas(int n,int c) {
+		vector<int> v(n, c);
+	}
+
+	Mas(int arr[], int n) {
+		vector<int> v(n);
+		for (int i = 0; i < n; i++) {
+			v[i] = arr[i];
+		}
+	}
+	
+	void change(int array[],int n) {
+		for (int i = 0; i < n; i++,n--) {
+			v[i] = array[n-1];
+			cout << v[i] << ' ';
+		}
+	}
+	~Mas() {
+		cout << "end";
+	}
+
+};
+
+	int main()
+	{
+		int arr[4] = { 1,4,5,8 };
+		Mas w(4,0);
+//	Mas w(arr, 4);
+		w.change(arr, 4);
+		return 0;
+	}
